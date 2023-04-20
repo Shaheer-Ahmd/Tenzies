cc92356315f1aca81fc9c117137eb99cde50b4ca
Shaheer-Ahmd <shaheerahmad57575@gmail.com>
2023-04-20 17:46:55 +0500

    Before adding additional features

---
+++ src/Die.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./style.css"
+
+export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    return (
+        <div 
+            className="die-face" 
+            style={styles}
+            onClick={props.holdDice}
+        >
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    )
+}
